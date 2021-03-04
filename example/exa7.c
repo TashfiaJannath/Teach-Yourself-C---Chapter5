018f8e163019d0774a8762f83a815e56e16cc58e
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-05 00:16:59 +0800

    Create exa7.c

---
+++ example/exa7.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void){
+    char command[80],temp[80];
+    int i,j;
+
+    for( ; ; ){
+        printf("Operation? ");
+        gets(command);
+
+
+        /* see if user wants to stop*/
+        if(!strcmp(command,"quit"))break;
+
+        printf("Enter first number: ");
+        gets(temp);
+        i=atoi(temp);
+
+        printf("Enter 2nd number: ");
+        gets(temp);
+        j=atoi(temp);
+
+        /* now perform the operation */
+        if(!strcmp(command, "add"))
+        printf("%d\n", i+j);
+
+        else if(!strcmp(command, "subtract"))
+        printf("%d\n", i-j);
+
+        else if(!strcmp(command, "multiply"))
+        printf("%d\n", i*j);
+
+        else printf("Unknown command. \n");
+        }
+
+        return 0;
+
+        }
