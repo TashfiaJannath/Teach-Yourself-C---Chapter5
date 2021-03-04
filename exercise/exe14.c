900be06e6785ca03a74a719211c8d128e5a821c1
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-05 00:42:46 +0800

    Create exe14.c

---
+++ exercise/exe14.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+
+    char str[80];
+    int spaces, periods, commas;
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    spaces = 0;
+    commas = 0;
+    periods = 0;
+
+
+    for(i=0; i<strlen(str); i++)
+        switch(str[i]) {
+            case '.': periods++;
+                break;
+            case ',': commas++;
+                break;
+            case ' ': spaces++;
+                break;
+    }
+
+
+    printf("spaces: %d\n", spaces);
+    printf("commas: %d\n", commas);
+    printf("periods: %d", periods);
+
+    return 0;
+    }
